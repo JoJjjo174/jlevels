5646c3e93498df428cda0d3e7873bcb7efcb924e
Jonathan <95241309+JoJjjo174@users.noreply.github.com>
2026-03-17 15:40:55 +0100

    added player argument to GiveBottleCommand

--- src/main/java/me/jojjjo147/jLevels/commands/GiveBottleCommand.java
+++ src/main/java/me/jojjjo147/jLevels/commands/GiveBottleCommand.java
@@ -1,6 +1,7 @@
 package me.jojjjo147.jLevels.commands;
 
 import me.jojjjo147.jLevels.JLevels;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -26,41 +27,53 @@ public class GiveBottleCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
 
-        if (commandSender instanceof Player p) {
-
-            if (args.length > 1) {
-
-                ItemStack item = new ItemStack(Material.EXPERIENCE_BOTTLE);
-                ItemMeta meta = item.getItemMeta();
-                PersistentDataContainer data = meta.getPersistentDataContainer();
-
-                try {
-                    data.set(new NamespacedKey(plugin, "jxp"), PersistentDataType.INTEGER, Integer.valueOf(args[0]));
-
-                    meta.setDisplayName(plugin.getMessage("name-xpbottle").replace("%xp%", args[0]));
-                    meta.setLore(Arrays.asList(
-                            plugin.getMessage("lore-xpbottle").replace("%xp%", args[0])
-                    ));
-
-                    item.setItemMeta(meta);
-                    item.setAmount(Integer.valueOf(args[1]));
-
-                    p.getInventory().addItem(item);
-
-                    commandSender.sendMessage(plugin.getMessage("message-xpbottle-given"));
-
-                } catch (NumberFormatException e) {
-                    commandSender.sendMessage(plugin.getMessage("message-xp-number"));
-                }
-
-            } else {
-                commandSender.sendMessage(plugin.getMessage("message-missing-arguments"));
-            }
-
-        } else {
-            commandSender.sendMessage(plugin.getMessage("command-not-player"));
+        if (args.length < 3) {
+            commandSender.sendMessage(plugin.getMessage("message-missing-arguments"));
+            return true;
         }
 
+        String targetPlayerArgument = args[0];
+        String stringXpArgument = args[1];
+        String stringAmountArgument = args[2];
+
+        int xpArgument, amountArgument;
+        try {
+            xpArgument = Integer.parseInt(stringXpArgument);
+            amountArgument = Integer.parseInt(stringAmountArgument);
+        } catch (NumberFormatException e) {
+            commandSender.sendMessage(plugin.getMessage("message-xp-number"));
+            return true;
+        }
+
+        Player target = Bukkit.getServer().getPlayerExact(targetPlayerArgument);
+        if (target == null) {
+            commandSender.sendMessage(plugin.getMessage("message-invalid-player"));
+            return true;
+        }
+
+        ItemStack item = new ItemStack(Material.EXPERIENCE_BOTTLE);
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer data = meta.getPersistentDataContainer();
+
+        data.set(new NamespacedKey(plugin, "jxp"), PersistentDataType.INTEGER, xpArgument);
+
+        meta.setDisplayName(plugin.getMessage("name-xpbottle").replace("%xp%", stringXpArgument));
+        meta.setLore(Arrays.asList(
+                plugin.getMessage("lore-xpbottle").replace("%xp%", stringXpArgument)
+        ));
+
+        item.setItemMeta(meta);
+        item.setAmount(amountArgument);
+
+        target.getInventory().addItem(item);
+
+        commandSender.sendMessage(applyPlaceholders(plugin.getMessage("message-xpbottle-given"), target, amountArgument));
         return true;
+    }
+
+    private String applyPlaceholders(String message, Player player, int amount) {
+        return message
+                .replace("%player%", player.getDisplayName())
+                .replace("%amount%", Integer.toString(amount));
     }
 }
