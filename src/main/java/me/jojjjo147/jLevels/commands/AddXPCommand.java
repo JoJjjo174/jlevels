51b1b6e4a2151ebd6c57646645fffe4389b593f0
Jonathan <95241309+JoJjjo174@users.noreply.github.com>
2026-03-18 18:52:40 +0100

    tabcomplete and refactor of /addxp

--- src/main/java/me/jojjjo147/jLevels/commands/AddXPCommand.java
+++ src/main/java/me/jojjjo147/jLevels/commands/AddXPCommand.java
@@ -7,9 +7,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class AddXPCommand implements CommandExecutor {
+import java.util.List;
+
+public class AddXPCommand implements CommandExecutor, TabExecutor {
 
     private final JLevels plugin;
 
@@ -19,50 +24,51 @@ public class AddXPCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        if (commandSender instanceof Player p) {
-            if (args.length > 1) {
 
-                Player target = Bukkit.getServer().getPlayerExact(args[0]);
-                try {
-                    int xpAmount = Integer.parseInt(args[1]);
-
-                    if (target != null) {
-
-                        String xpReason = "";
-
-                        if (args.length > 2) {
-
-                            for (int i = 2; i < args.length; i++) {
-
-                                xpReason += args[i] + " ";
-                            }
-
-                            xpReason = xpReason.substring(0, xpReason.length() - 1);
-
-                        } else {
-                            xpReason = plugin.getMessage("xpreason-admin");
-
-                        }
-
-                        plugin.getXpManager().addXP(target, xpAmount, xpReason);
-
-                        commandSender.sendMessage(plugin.getMessage("message-add-xp"));
-
-                    } else {
-                        commandSender.sendMessage(plugin.getMessage("message-invalid-player"));
-                    }
-
-                } catch (NumberFormatException e) {
-                    commandSender.sendMessage(plugin.getMessage("message-xp-number"));
-                }
-
-            } else {
-                commandSender.sendMessage(plugin.getMessage("message-missing-arguments"));
-            }
-        } else {
-            commandSender.sendMessage(plugin.getMessage("command-not-player"));
+        if (args.length < 2) {
+            commandSender.sendMessage(plugin.getMessage("message-missing-arguments"));
+            return true;
         }
 
+        Player target = Bukkit.getServer().getPlayerExact(args[0]);
+        if (target == null) {
+            commandSender.sendMessage(plugin.getMessage("message-invalid-player"));
+            return true;
+        }
+
+        String stringXpArgument = args[1];
+        int xpArgument;
+        try {
+            xpArgument = Integer.parseInt(stringXpArgument);
+        } catch (NumberFormatException e) {
+            commandSender.sendMessage(plugin.getMessage("message-xp-number"));
+            return true;
+        }
+
+        String xpReason;
+        if (args.length > 2) {
+            StringBuilder xpReasonBuilder = new StringBuilder();
+
+            for (int i = 2; i < args.length; i++) {
+                xpReasonBuilder.append(args[i]).append(" ");
+            }
+
+            xpReason = xpReasonBuilder.toString().strip();
+        } else {
+            xpReason = plugin.getMessage("xpreason-admin");
+        }
+
+        plugin.getXpManager().addXP(target, xpArgument, xpReason);
+        commandSender.sendMessage(plugin.getMessage("message-add-xp"));
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        return switch (args.length) {
+            case 1 -> null;
+            case 2 -> List.of("<xp>");
+            default -> List.of("[message]");
+        };
     }
 }
